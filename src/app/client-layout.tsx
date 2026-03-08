305ac82964718619b637eac4779e2b927b037f88
code-with-altaf <reachmohdaltaf@gmail.com>
2026-03-08 22:35:50 +0530

    chcekc

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,12 +1,36 @@
 "use client";
 
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import LenisProvider from "@/components/lenis-provider";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Providers } from "./providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const styrene = localFont({
+    src: [
+        {
+            path: "../../public/font/styrene-font-family/StyreneA-Regular-Trial-BF63f6cbd970ee9.otf",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "../../public/font/styrene-font-family/StyreneA-Medium-Trial-BF63f6cbdb24b6d.otf",
+            weight: "500",
+            style: "normal",
+        },
+        {
+            path: "../../public/font/styrene-font-family/StyreneA-Bold-Trial-BF63f6cbda1877f.otf",
+            weight: "700",
+            style: "normal",
+        },
+        {
+            path: "../../public/font/styrene-font-family/StyreneA-Black-Trial-BF63f6cbd9da245.otf",
+            weight: "900",
+            style: "normal",
+        },
+    ],
+    display: "swap",
+});
 
 export default function ClientLayout({
     children,
@@ -18,7 +42,7 @@ export default function ClientLayout({
     messages: any;
 }) {
     return (
-        <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
+        <body className={`bg-[#FCFCFC] dark:bg-black ${styrene.className}`}>
             <Providers>
                 <LenisProvider>
                     <div>
